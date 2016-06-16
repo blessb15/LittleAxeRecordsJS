de6356c154125eb1783a5ef7f30fed931e80405a
Blake Bless and Ryan Carlile <Guest@epicodus-87.local>
2016-06-16 10:12:36 -0700

    display items in shopping cart and give you the option to remove and add items.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('record', {path: 'record/:id'}) ;
+  this.route('contact');
 });
 
 export default Router;
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  items: [],
+items: [],
 
   add(item) {
-    console.log(item)
     this.get('items').pushObject(item);
+  },
+
+  remove(item) {
+    this.get('items').removeObject(item);
   }
 });
